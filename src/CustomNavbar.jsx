9860446ec3ad34f1a853a9029dabe26adb142922
YoselinMejia <20011412@galileo.edu>
2024-05-16 23:52:48 -0600

    Terminado

--- src/CustomNavbar.jsx
+++ src/CustomNavbar.jsx
@@ -6,14 +6,14 @@ import './styles.css';
 const CustomNavbar = ({ cities }) => {
     
   return (
-    <Navbar className="navbar-css" bg="light" expand="lg">
-      <Navbar.Brand as={Link} to="/">City Explorer</Navbar.Brand>
+    <Navbar className="navbarcss" bg="light" expand="lg">
+      <Navbar.Brand as={Link} to="/" className="navbar-css" >City Explorer</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="mr-auto">
-        <Nav.Link as={Link} to="/">Home</Nav.Link>
+        <Nav.Link as={Link} to="/" className="nav-link-custom" >Home</Nav.Link>
         {cities.map(city => (
-          <Nav.Link key={city} as={Link} to={`/city/${city}`}>{city}</Nav.Link>
+          <Nav.Link key={city} as={Link} to={`/city/${city}`} className="nav-link-custom" >{city}</Nav.Link>
         ))}
       </Nav>
       </Navbar.Collapse>
